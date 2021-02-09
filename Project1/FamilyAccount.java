a37ff1e7d4d91411f7140a3bac983576e0d6721a
LeonLin0516 <52585348+LeonLin0516@users.noreply.github.com>
2021-02-09 07:58:58 -0800

    completed Project1

--- Project1/FamilyAccount.java
+++ Project1/FamilyAccount.java
@@ -2,6 +2,10 @@ public class FamilyAccount {
   
   public static void main(String[] args) {
     boolean isFlag = true;
+    String details = "Income/Expense\tAccount\tAmount\tDescription\n";
+    int balance = 0;
+    int amount;
+    String info;
     while (isFlag) {
 
       System.out.print("---------------Family Account---------------\n");
@@ -16,16 +20,32 @@ public class FamilyAccount {
       case '1':
         System.out.println();
         System.out.println("----Current Income & Expenditure Details----");
-        System.out.println("Income/Expense\tAccount\tAmount\tDescription\n");
+        System.out.println(details);
         System.out.println("--------------------------------------------");
         break;
 
       case '2':
-        System.out.println('2');
+        System.out.print("Income Amount: ");
+        amount = Utility.readNumber();
+        System.out.print("Description: ");
+        info = Utility.readString();
+        System.out.println("-------------------Completed----------------\n");
+        balance += amount;
+        details += "Income\t\t" + balance + "\t" + amount + "\t" + info + "\n";
         break;
 
       case '3':
-        System.out.println('3');
+        System.out.print("Expense Amount: ");
+        amount = Utility.readNumber();
+        System.out.print("Description: ");
+        info = Utility.readString();
+        if (balance >= amount) {
+          balance -= amount;
+          details += "Expense\t\t" + balance + "\t" + amount + "\t" + info + "\n";
+        } else {
+          System.out.println("Not enough money in the account!\n");
+        }
+        System.out.println("-------------------Completed----------------\n");
         break;
 
       case '4':
